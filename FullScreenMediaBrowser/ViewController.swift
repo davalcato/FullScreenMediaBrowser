afb34e4df1ae606822c9c9f8947edb20ecbedc13
davalcato <davalcato@yahoo.com>
2017-03-17 16:28:51 -0700

    Initial Commit

---
+++ FullScreenMediaBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FullScreenMediaBrowser
+//
+//  Created by Daval Cato on 3/17/17.
+//  Copyright © 2017 Daval Cato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
